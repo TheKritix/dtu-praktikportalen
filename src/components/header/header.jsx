92bb451e9b68984a3164aa7691e80e49eca5c7cd
Kristoffer <kristoffer@kritix.dk>
2022-09-04 17:16:47 +0200

    Initial work on header.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./header.css";
+import dtulogo from "../../images/dtu-logo.png";
 
 export const Header = () => {
-    return (
-        <div>
-            oqwheojqhwe
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <img className="dtulogo" src={dtulogo} alt="DTU-Logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
