9f1e7583c9bcbdbefd66768821b8341135fb5d44
Kongsbak99 <victoreskongsbak@gmail.com>
2022-09-20 13:35:32 +0200

    -

--- src/App.js
+++ src/App.js
@@ -6,29 +6,17 @@ import Navbar from './components/navbar/navbar.jsx'
 import Header from './components/header/header.jsx'
 
 
-// XXXX IMAGES XXXX
-
-
-
-//XXXX Bootstrap XXXX
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-
 function App() {
 
   return (
     <BrowserRouter>
-
+      <Navbar/>
       <Routes>
-        <Route path="/" element={Header}/>
+        <Route path="/" element={<Header/>}/>
       </Routes>
 
 
-      <Navbar/>
-      <Header/>
+      
 
     </BrowserRouter>
     
