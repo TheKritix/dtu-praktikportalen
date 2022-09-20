e5d6f8a6c86212d26b55121236f4f1be61c7158a
Kristoffer <kristoffer@kritix.dk>
2022-09-20 23:44:11 +0200

    Worked on the profile page. Bit of a pain without a matching backend..

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // XXXX COMPONENTS XXXX
 import Navbar from './components/navbar/navbar.jsx'
 import Header from './components/header/header.jsx'
+import Profile from './components/profile/profile.jsx'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Header/>}/>
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,18 +1,89 @@
 import React, { useState } from "react";
 import "./profile.css";
+import "@fontsource/poppins";
+import Form from "react-bootstrap/Form";
+
+import image from "./profileTestImage.jpg";
 
 const Profile = () => {
+  const [view, setView] = useState("profile");
+  const [name, setName] = useState("PLACEHOLDER NAME");
 
-  const [title, setTitle] = useState("Profilindstillinger");
+  const ProfileSettings = () => {
+    return (
+      <div>
+        <div className="profileBackdrop"></div>
+        <div className="profilePicture">
+          <img src={image} className="image" alt="profilbillede" />
+        </div>
+        <p className="name">{name}</p>
+        <p className="description">Student</p>
+
+        <div className="namebox-container">
+          <p className="textbox-name">Navn</p>
+          <Form.Control className="name-textbox" placeholder="Navn" />
+        </div>
+        <div className="cv-container">
+          <p className="textbox-cv">Resumé</p>
+          <Form.Control className="cv-textbox" placeholder="Navn" />
+        </div>
+
+      </div>
+    );
+  };
+
+  const EmailSettings = () => {
+    return <div>Test</div>;
+  };
+
+  function getView() {
+    let ViewComponent;
+
+    switch (view) {
+      case "profile":
+        ViewComponent = ProfileSettings();
+        break;
+      case "email":
+        ViewComponent = EmailSettings();
+        break;
+      default:
+        ViewComponent = ProfileSettings();
+        break;
+    }
+
+    return ViewComponent;
+  }
 
   return (
-    <div>
+    <div className="profile-container">
       <div className="profileTitle">
-        <p className="title">{title}</p>
+        <p className="title">Profilindstillinger</p>
       </div>
       <div className="profile">
-        <div className="profileMenu"></div>
-        <div className="profileMain"></div>
+        <div className="profileMenu">
+          <button
+            style={{
+              fontWeight: view === "profile" ? "bold" : "normal",
+            }}
+            onClick={() => setView("profile")}
+            className="profileButton"
+          >
+            Profil
+          </button>
+          <button
+            style={{
+              fontWeight: view === "email" ? "bold" : "normal",
+            }}
+            onClick={() => setView("email")}
+            className="profileButton"
+          >
+            Email
+          </button>
+          <button className="profileButton">Password</button>
+          <button className="profileButton">Notifikationer</button>
+          <button className="profileButton">Kontakt</button>
+        </div>
+        <div className="profileMain">{getView()}</div>
       </div>
     </div>
   );
