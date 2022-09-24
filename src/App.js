88178f719c8e9c581fce3442d274d2ff3b93e9cb
Kristoffer <kristoffer@kritix.dk>
2022-09-24 22:31:46 +0200

    Sorted the annoying issue with Pages.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 // XXXX COMPONENTS XXXX
-import Navbar from './components/navbar/navbar.jsx'
-import Header from './components/header/header.jsx'
-import Profile from './components/profile/profile.jsx'
-
+import Navbar from "./components/navbar/navbar.jsx";
+import Header from "./components/header/header.jsx";
+import Profile from "./components/profile/profile.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Header/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<Header />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dtu-praktikportalen" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
