82cbf925518ac4d4bc755bd97455823ba9becdcb
lauritzpepke <s191179@student.dtu.dk>
2022-09-24 12:32:19 +0200

    123

--- src/components/create-post/inputForm.jsx
+++ src/components/create-post/inputForm.jsx
@@ -8,39 +8,47 @@ const InputForm = () => {
 
     return (
         <div className="form-container">
-            <Form>
-                <Form.Group className="create-group1">
-                    <Form.Label className="form-titel-text">Jobtitel</Form.Label>
-                    <Form.Control className="form-input" type="title" placeholder="Titel på stillingen"></Form.Control>
-                    
-                    <Form.Label className="form-titel-text">Stillingstype</Form.Label>
-                    <Form.Select className="form-input" placeholder="vælg stillingens type">
-                        <option>Elev</option>
-                        <option>Praktik</option>
-                        <option>Fuldtid</option>
-                        <option>Deltid</option>
-                    </Form.Select>
+            <div className="left-input-content">
+                <Form>
+                    <Form.Group className="create-group1">
+                        
+                        <h3 className="form-titel-text">Jobtitel</h3>
+                        <Form.Control className="form-input" type="title" placeholder="Titel på stillingen"></Form.Control>
+                        
+                        <h3 className="form-titel-text">Stillingstype</h3>
+                        <Form.Select className="form-input" placeholder="vælg stillingens type">
+                            <option value="">Vælg type</option>
+                            <option value="elev">Elev</option>
+                            <option value="praktik">Praktik</option>
+                            <option value="fuldtid">Fuldtid</option>
+                            <option value="deltid">Deltid</option>
+                        </Form.Select>
 
-                    <Form.Label className="form-titel-text">Firmanavn</Form.Label>
-                    <Form.Control className="form-input" type="title" placeholder="Navn på firmaet"></Form.Control>
-                    
-                    <Form.Label className="form-titel-text">Placering</Form.Label>
-                    <Form.Control className="form-input" type="title" placeholder="f.eks. København"></Form.Control>
+                        <h3 className="form-titel-text">Firmanavn</h3>
+                        <Form.Control className="form-input" type="title" placeholder="Navn på firmaet"></Form.Control>
+                        
+                        <h3 className="form-titel-text">Placering</h3>
+                        <Form.Control className="form-input" type="title" placeholder="f.eks. København"></Form.Control>
 
-                    <Form.Label className="form-titel-text">Kontaktperson</Form.Label>
-                    <Form.Control className="form-input" type="title" placeholder="Navn på kontaktperson"></Form.Control>
+                        <h3 className="form-titel-text">Kontaktperson</h3>
+                        <Form.Control className="form-input" type="title" placeholder="Navn på kontaktperson"></Form.Control>
 
-                    <Form.Label className="form-titel-text">Email</Form.Label>
-                    <Form.Control className="form-input" type="email" placeholder="Indtast Email"></Form.Control>
+                        <h3 className="form-titel-text">Email</h3>
+                        <Form.Control className="form-input" type="email" placeholder="Indtast Email"></Form.Control>
 
-                    <Form.Label className="form-titel-text">Telefon</Form.Label>
-                    <Form.Control className="form-input" type="number" placeholder="+45"></Form.Control>
+                        <h3 className="form-titel-text">Telefon</h3>
+                        <Form.Control className="form-input" type="number" placeholder="+45"></Form.Control>
 
-                </Form.Group>
-                <Button className="preview-button" size="lg">Preview opslag</Button>
-                <Button className="form-submit-button" type="submit" size="lg">Opret stilling</Button>
-            
-            </Form>
+                    </Form.Group>
+                    <Button className="preview-button" size="lg">Preview opslag</Button>
+                    <Button className="form-submit-button" type="submit" size="lg">Opret stilling</Button>
+                
+                </Form>
+            </div>
+            <div className="right-input-content">
+                
+                        prut
+            </div>
         </div>
     );
 }
