539b613bf0841df9f3dad5a0bc6a39e2045b4cba
oklenum <oliverbrinkklenum@gmail.com>
2022-09-24 23:38:12 +0200

    pretotype-login

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import login from "../login/login";
+import LoginEmployee from "../login/login";
 
 
 //XXXX IMAGES XXXX
@@ -18,22 +18,27 @@ import employee from '../../res/images/employee.png'
 
 const Header = () => {
   const [showLogin, setShowLogin] = useState(false);
-  const toggleModal = () => { 
-    setShowLogin(false);
-  }
+  const handleClose = () => setShowLogin(false);
+  const handleShow = () => setShowLogin(true);
   return (
     <div>
       <Container className='pt-3 w-100 overflow-hidden' fluid='true'>
         <Row className='overflow-hidden'>
           <Col sm={5} className='mb-5'>
             <div className="mx-5">
-              <login show={showLogin} onHide={showLogin}/>
               <p className='pre-h-text'>Find din drøme praktikplads</p>
               <h1 className='landingpage-header'>Skab kontakt til virksomheder</h1>
               <p className='header-text'>Login med Campus Net og begyndt din søgen efter praktikophold gennem vores brugervenlige portal</p>
               <div className="d-flex flex-row landingpage-buttons">
-                <button className='me-2 student'><img src={student}/>Student <span>Login</span></button>
-                <button onClick={toggleModal()} className='ms-5 employee'><img src={employee}/>Employee <span>Login</span></button>
+                <a href="/profile">
+                  <button className='me-2 student'>
+                    <img src={student}/>Student <span>Login</span>
+                  </button>
+                </a>
+                
+                <button className='ms-5 employee' onClick={()=> handleShow()}>
+                  <img src={employee}/>Employee <span>Login</span>
+                </button>
               </div>
             </div>
             
@@ -48,6 +53,8 @@ const Header = () => {
         <p className='mx-auto mt-5'>Se alle opslag</p>
         <h2 className='mx-auto'>Udvalgte Praktik Pladser</h2>
       </div>
+      <LoginEmployee show={showLogin} handleClose={handleClose} handleShow={handleShow}/>
+
     </div>
     
   );
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,14 +9,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useState } from "react";
+import { Button } from "react-bootstrap";
+
 
 
 const Navbar = () => {
-  const [showLogin, setShowLogin] = useState(false);
-  const toggleModal = () => { 
-    setShowLogin(true);
-  }
+ 
   return (
     <Container className="mb-5 mt-5 ms-3 me-3 w-auto p-0 overflow-hidden" fluid>
       <Row>
@@ -32,7 +30,7 @@ const Navbar = () => {
             <a style={{ textDecoration: 'none', color: 'black'}} className=" me-5 mt-1 ms-auto">Praktik pladser</a>
             <a style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Info</a>
             <Link to="/profile" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Profil(TEMP)</Link>
-            <button className="bg-white p-2 rounded-3 me-2">Login Campus Net</button>
+            <Button variant="outline-danger" href="https://auth.dtu.dk/dtu/login?menulanguage=da-DK">Login Campus Net</Button>
           </div>
         </Col>
       </Row>
