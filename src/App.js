1e11c6039cade55827013a02b0b01003c05b67c5
Kristoffer <kristoffer@kritix.dk>
2022-09-24 23:37:30 +0200

    small commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // XXXX COMPONENTS XXXX
 import Navbar from "./components/navbar/navbar.jsx";
