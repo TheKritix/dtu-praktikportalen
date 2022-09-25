d088e85ddacf17c34c3aa005d5dcafda78c4b783
Kristoffer <kristoffer@kritix.dk>
2022-09-25 19:02:17 +0200

    added some stuff

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -30,6 +30,8 @@ const Navbar = () => {
             <Link to="/" style={{ textDecoration: 'none', color: 'black'}} className=" me-5 mt-1 ms-auto">Praktik pladser</Link>
             <Link to="/" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Info</Link>
             <Link to="/profile" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Profil(TEMP)</Link>
+            <Link to="/post" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Post(TEMP)</Link>
+            <Link to="/createpost" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">CreatePost(TEMP)</Link>
             <Button variant="outline-danger" href="https://auth.dtu.dk/dtu/login?menulanguage=da-DK">Login Campus Net</Button>
           </div>
         </Col>
