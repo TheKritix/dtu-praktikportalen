4f39006067196ebd4fbd0a359cfece5c15d4e197
lauritzpepke <s191179@student.dtu.dk>
2022-09-25 13:32:10 +0200

    further

--- src/components/post-page/postContent.jsx
+++ src/components/post-page/postContent.jsx
@@ -2,24 +2,25 @@ import "./postContent.css";
 
 
 
-const PostContent = () => {
+const PostContent = ({post}) => {
 
-    
+
 
     return (
         <div className="post-left-column">
-            <div className="post-banner">
-                <h1 className="post-banner-text">Banner for stilling/firma</h1>   
+            <div className="post-banner-div">
+                <h1 className="post-banner-text">Placeholder Banner</h1>
+                <img className="post-banner" src={post[0].bannerImg}></img>    
             </div>
             <div className="post-content">
                 <div>
-                    <h3>Post navn (stilling)</h3>
+                    <h3>{post[0].position}</h3>
                     <hr className="post-divider"></hr>
-                    <h5>Type - Tidsramme - Placering</h5>
+                    <h5>{post[0].type} - {post[0].timeframe} - {post[0].location}</h5>
                 </div>
                 <div className="post-overview">
                     <h4>Overblik</h4>
-                    <h6 className="post-description">Her kommer der en beskrivelse af stillingen</h6>
+                    <h6 className="post-description">{post[0].description}</h6>
                 </div>
             </div>
         </div>
---
+++ src/components/post-page/postItem.jsx
@@ -0,0 +1,21 @@
+import bannerPlaceholder from "../../res/images/PlaceholderBanner.png"
+
+export const postItem = [
+    {
+      postId: "1",
+      createdAtDate: "25-09-2022",
+      position: "Studentermedhjælper",
+      type: "Praktik",
+      description: "Vi leder efter en studentmedhjælper der kan smørre madder og lave kaffe",
+      bannerImg: bannerPlaceholder,
+      location: "København",
+      country: "Denmark",
+      timeframe: "Oktober 2022",
+      company: "firma.com",
+      contact: "Per Son",
+      phonenumber: "10203040",
+      email: "person@mail.com"
+    },
+];
+
+export default postItem
