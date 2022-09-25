10421411b4f5ea39b56ce02ba27ea328b179938d
Rasmus Pedersen <rasmus.jdp@hotmail.com>
2022-09-25 19:33:04 +0200

    Fixed unused imports

--- src/components/internship-list/internship-list.jsx
+++ src/components/internship-list/internship-list.jsx
@@ -1,11 +1,10 @@
-import React, {Component} from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card'
-import {Button, Form} from "react-bootstrap";
+import Form from "react-bootstrap";
 
 
 const internshipList = [
