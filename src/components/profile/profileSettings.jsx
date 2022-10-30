802b7bcbf05770c0f05ead1f23bcac8bb21bc242
Kristoffer <kristoffer@kritix.dk>
2022-10-30 20:33:10 +0100

    A lot of work on intial cropping and uploading

--- src/components/profile/profileSettings.jsx
+++ src/components/profile/profileSettings.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+
 import "./profileSettings.css";
+import "./profileImagePopup.css";
 import "@fontsource/poppins";
 import Form from "react-bootstrap/Form";
+
+// Source: https://www.npmjs.com/package/react-image-crop
+// Source: https://www.npmjs.com/package/reactjs-popup
+import ReactCrop, { makeAspectCrop } from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 // Profil billede source: https://www.reddit.com/r/DANMAG/comments/2cevxx/vores_allesammens_kronprins_frederik/
 import image from "./profileTestImage.jpg";
@@ -9,13 +18,69 @@ import addImage from "../../res/images/add-image.png";
 
 // Textbox Source: https://react-bootstrap.github.io/forms/form-control/
 
-
 const ProfileSettings = () => {
-  //const [name, setName] = useState("PLACEHOLDER NAME");
+  const inputRef = useRef(null);
+  const [crop, setCrop] = useState();
+
+  const [uploadBackdropImage, setUploadBackdropImage] = useState();
+  const [finishedCrop, setFinishedCrop] = useState();
+
+  const [backdropUploadState, setBackdropUploadState] = useState(false);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleFileChangeBackdrop = (e) => {
+    console.log(e.target.files[0]);
+    if (!e.target.files[0]) {
+      return;
+    } else {
+      const imgObject = new Image();
+
+      imgObject.src = URL.createObjectURL(e.target.files[0]);
+
+      imgObject.onload = () => {
+        setCrop(
+          makeAspectCrop(
+            {
+              unit: "%",
+              width: 50,
+            },
+            5 / 1,
+            imgObject.width,
+            imgObject.height
+          )
+        );
+      };
+
+      setUploadBackdropImage(imgObject.src);
+      setBackdropUploadState(true);
+    }
+  };
+
+  const ClearPopup = () => {
+    setBackdropUploadState(false);
+  };
+
+  const CompleteCrop = () => {};
+
   return (
     <div>
-      <div className="profileBackdrop" onClick={true /* Do something */}>
-        {image && <img src={image} alt="profileBackDropImage" className="backDropImage"/>}
+      <div className="profileBackdrop" onClick={handleClick}>
+        <input
+          style={{ display: "none" }}
+          ref={inputRef}
+          type="file"
+          onChange={handleFileChangeBackdrop}
+        />
+        {image && (
+          <img
+            src={image}
+            alt="profileBackDropImage"
+            className="backDropImage"
+          />
+        )}
         <img className="backDropImageAdd" alt="addImage icon" src={addImage} />
       </div>
       <div className="profilePicture">
@@ -47,6 +112,33 @@ const ProfileSettings = () => {
       <div className="saveButton">
         <button className="save">Gem</button>
       </div>
+
+      
+
+      {/* Det her er til popup når man skal uploade et billede. */}
+      {uploadBackdropImage && (
+        <Popup
+          open={backdropUploadState}
+          position="right center"
+          onClose={ClearPopup}
+        >
+          <div className="popupContainer">
+            <div className="popupImage">
+              <ReactCrop
+                crop={crop}
+                onChange={(c) => setCrop(c)}
+                onComplete={(c) => setFinishedCrop(c)}
+                aspect={5 / 1}
+              >
+                <img src={uploadBackdropImage} alt="nah" />
+              </ReactCrop>
+            </div>
+            <button className="popupButton" onClick={ClearPopup}>
+              Gem
+            </button>
+          </div>
+        </Popup>
+      )}
     </div>
   );
 };
---
+++ src/components/profile/returnCrop.jsx
@@ -0,0 +1,2 @@
+//https://codesandbox.io/s/react-image-crop-demo-with-react-hooks-forked-n7xcpl?file=/src/canvasPreview.ts
+//https://www.npmjs.com/package/react-image-crop#how-can-i-center-the-crop
