4cb2d255e63c4f105c860ae3901448cfe84b6e7c
Oliver Brink Klenum <oliverbrinkklenum@gmail.com>
2022-11-01 19:18:23 +0100

    employer login and validation

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import "./profile.css";
 import "@fontsource/poppins";
 import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
 import ProfileSettings from "./profileSettings.jsx";
-
+import authService from "../../services/auth-service";
+import { useEffect } from "react";
 // Textbox Source: https://react-bootstrap.github.io/forms/form-control/
 
 const Profile = () => {
   const [view, setView] = useState("profile");
 
   const [email, setEmail] = useState("foo@boo.com");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = authService.getCurrentUser();
+    if (user) {
+      console.log(user.username);
+    } else {
+      navigate("/");
+    }
+  }, [navigate]);
 
   //Not needed for anything, and probably won't be needed for anything
   const handleChange = (e) => {
     e.preventDefault();
-    setEmail(e.target.value);    
+    setEmail(e.target.value);
   };
 
   const EmailSettings = () => {
@@ -24,7 +35,11 @@ const Profile = () => {
           <p className="textbox-main">Email</p>
           <Form.Control className="main-textbox" placeholder={email} disabled />
           <p className="textbox-main">Skift Email-Adresse</p>
-          <Form.Control className="main-textbox" placeholder="Ny Email" onChange={handleChange}/>
+          <Form.Control
+            className="main-textbox"
+            placeholder="Ny Email"
+            onChange={handleChange}
+          />
           <div className="saveButton">
             <button className="saveMain">Gem</button>
           </div>
@@ -38,9 +53,17 @@ const Profile = () => {
       <div>
         <div className="main-container">
           <p className="textbox-main">Skift Password</p>
-          <Form.Control className="main-textbox" type="password" placeholder="Ny Password" />
+          <Form.Control
+            className="main-textbox"
+            type="password"
+            placeholder="Ny Password"
+          />
           <p className="textbox-main">Gentag Password</p>
-          <Form.Control className="main-textbox" type="password" placeholder="Gentag Password" />
+          <Form.Control
+            className="main-textbox"
+            type="password"
+            placeholder="Gentag Password"
+          />
           <div className="saveButton">
             <button className="saveMain">Gem</button>
           </div>
