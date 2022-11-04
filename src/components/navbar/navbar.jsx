1681b112b68bc7c3c44b615445844835c0aeba45
Oliver Brink Klenum <oliverbrinkklenum@gmail.com>
2022-11-04 16:11:03 +0100

    student-login

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,52 +1,90 @@
 import React, { useEffect } from "react";
 import "./navbar.css";
 import dtulogo from "../../res/images/dtu-logo.png";
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from "react-router-dom";
 import authService from "../../services/auth-service";
 import { useNavigate } from "react-router-dom";
 
-
 //XXXX Bootstrap XXXX
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
-
-
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [searchParam] = useSearchParams();
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [newStudent, setNewStudent] = useState(false);
   useEffect(() => {
     const user = authService.getCurrentUser();
     if (user) {
-      setCurrentUser(user.username);
+      setCurrentUser(user.email);
+      console.log(currentUser);
     } else {
       setCurrentUser(undefined);
     }
   }, []);
 
- useEffect(() => {
+  useEffect(() => {
     searchParam.get("ticket");
     const currentUser = authService.getCurrentUser();
-  }, [searchParam]);
-  const handleLogin = async () => {
-    await authService.studentSignin(searchParam.get("ticket")).then(
-      (response) => {
-        console.log(response.data);
-        
+    console.log(searchParam.get("ticket"));
+    if (searchParam.get("ticket") != null && currentUser == null) {
+      authService.studentLogin(searchParam.get("ticket")).then(
+        () => {
+          console.log("student signup");
+          navigate("/profile");
+          window.location.reload();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+      //handleLogin();
+    }
+  }, [searchParam, navigate]);
+  /*
+  const handleLogin = async (e) => {
+    //e.preventDefault();
+    authService.studentSignup(searchParam.get("ticket")).then(
+      () => {
+        authService.studentLogin().then(
+          () => {
+            navigate("/profile");
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+        //window.location.reload();
       },
       (error) => {
         console.log(error);
       }
     );
-  }
+  };
+
+  /*
+  const handleLogin = (e) => {
+    authService.studentLogin(searchParam.get("ticket")).then(
+      () => {
+        console.log("Logged in");
+        //navigate("/profile");
+        //window.location.reload();
+      },
+      (error) => {
+        console.log(error.toString());
+      }
+    );
+  };
+  */
 
   const handleLogout = () => {
-    navigate("/")
+    navigate("/");
     authService.logout();
     window.location.reload();
   };
@@ -55,22 +93,63 @@ const Navbar = () => {
       <Row>
         <Col>
           <div sm={8} className="d-flex flex-row">
-            <Link to="/"><img className="dtulogo ms-4" src={dtulogo} alt="DTU-Logo" /></Link>
+            <Link to="/">
+              <img className="dtulogo ms-4" src={dtulogo} alt="DTU-Logo" />
+            </Link>
             <h4 className="d-flex flex-row mt-4 ms-5">DTU Praktikportalen</h4>
           </div>
         </Col>
         <Col>
           <div className="d-flex flex-row mt-4 ms-auto me-2">
             {/* <a> DISSE BURDE VÆRE <Link> */}
-            <Link to="/internships" style={{ textDecoration: 'none', color: 'black'}} className=" me-5 mt-1 ms-auto">Praktikpladser</Link>
-            <Link to="/" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Info</Link>
-            <Link to="/profile" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Profil(TEMP)</Link>
-            <Link to="/post" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">Post(TEMP)</Link>
-            <Link to="/createpost" style={{ textDecoration: 'none', color: 'black' }} className=" me-5 mt-1">CreatePost(TEMP)</Link>
-            {currentUser 
-            ? <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>
-            : <Button variant="outline-danger" href="https://auth.dtu.dk/dtu/?service=http://localhost:3001/dtu-praktikportalen" onClick={handleLogin}>Login Campus Net</Button>
-            }
+            <Link
+              to="/internships"
+              style={{ textDecoration: "none", color: "black" }}
+              className=" me-5 mt-1 ms-auto"
+            >
+              Praktikpladser
+            </Link>
+            <Link
+              to="/"
+              style={{ textDecoration: "none", color: "black" }}
+              className=" me-5 mt-1"
+            >
+              Info
+            </Link>
+            <Link
+              to="/profile"
+              style={{ textDecoration: "none", color: "black" }}
+              className=" me-5 mt-1"
+            >
+              Profil(TEMP)
+            </Link>
+            <Link
+              to="/post"
+              style={{ textDecoration: "none", color: "black" }}
+              className=" me-5 mt-1"
+            >
+              Post(TEMP)
+            </Link>
+            <Link
+              to="/createpost"
+              style={{ textDecoration: "none", color: "black" }}
+              className=" me-5 mt-1"
+            >
+              CreatePost(TEMP)
+            </Link>
+            {currentUser ? (
+              <Button variant="outline-danger" onClick={handleLogout}>
+                Logout
+              </Button>
+            ) : (
+              <Button
+                variant="outline-danger"
+                href="https://auth.dtu.dk/dtu/?service=http://localhost:3001/dtu-praktikportalen"
+                //onClick={handleLogin}
+              >
+                Login Campus Net
+              </Button>
+            )}
           </div>
         </Col>
       </Row>
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -2,29 +2,29 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000/api/auth/";
 
-const studentSignin = (ticket) => {
+const studentSignup = (ticket) => {
+  return axios.post(API_URL + "studentSignup", {
+    ticket,
+  });
+};
+
+const studentUpdate = (ticket) => {
+  return axios.post(API_URL + "studentUpdate", {
+    ticket,
+  });
+};
+
+const studentLogin = (ticket) => {
   return axios
-    .post(API_URL + "studentlogin", {
+    .post(API_URL + "studentLogin", {
       ticket,
     })
     .then((response) => {
+      console.log(response);
       if (response.data.accessToken) {
         localStorage.setItem("user", JSON.stringify(response.data));
       }
-      console.log(JSON.stringify(response.data));
-      return response.data;
-    });
-};
 
-const dtuCasLogin = (ticket) => {
-  return axios
-    .get("https://auth.dtu.dk/dtu/servicevalidate", {
-      params: {
-        service: "http://localhost:3001/dtu-praktikportalen",
-        ticket: ticket,
-      },
-    })
-    .then((response) => {
       return response.data;
     });
 };
@@ -54,6 +54,19 @@ const employerLogin = (username, password) => {
     });
 };
 
+const dtuCasLogin = (ticket) => {
+  return axios
+    .get("https://auth.dtu.dk/dtu/servicevalidate", {
+      params: {
+        service: "http://localhost:3001/dtu-praktikportalen",
+        ticket: ticket,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    });
+};
+
 const logout = () => {
   localStorage.removeItem("user");
 };
@@ -62,10 +75,11 @@ const getCurrentUser = () => {
   return JSON.parse(localStorage.getItem("user"));
 };
 
-
 const authService = {
-  studentSignin,
+  studentSignup,
   employerSignup,
+  studentLogin,
+  studentUpdate,
   employerLogin,
   logout,
   getCurrentUser,
