44f16306e2be4d26b52737b4e20b54919ff5ab9e
Oliver Brink Klenum <oliverbrinkklenum@gmail.com>
2022-11-08 18:23:49 +0100

    env for api's

--- src/services/student-service.js
+++ src/services/student-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/";
+const API_URL = process.env.REACT_APP_API;
 
 const studentPDFUpload = (pdf, user) => {
   var formData = new FormData();
