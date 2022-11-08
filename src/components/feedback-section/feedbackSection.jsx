b1ba2db5aa3509f04d9d7c273e51c693f2d4146d
Kongsbak99 <victoreskongsbak@gmail.com>
2022-11-08 09:36:00 +0100

    feedback fetch fixed

--- src/components/feedback-section/feedbackSection.jsx
+++ src/components/feedback-section/feedbackSection.jsx
@@ -1,24 +1,35 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import { observer, inject } from 'mobx-react';
 import "./feedbackStyles.css";
  
 import FeedbackCard from "./feedbackCard";
 //import feedbackData from "./feedbackData";
-import {feedbackStore} from "./feedbackData";
+import {feedbackStore} from "./feedbackStore";
 
 //import FeedbackService from './feedbackService';
 
 
-const FeedbackSection = () => {
+const FeedbackSection = () => { 
 
-    feedbackStore.fetchFeedback();
+    const [feedbacks, setFeedbacks] = useState();
 
-    console.log(feedbackStore.res);
+    const fs = feedbackStore;
+
+
+    useEffect(() => {
+        fs.fetchFeedback();
+        setFeedbacks(fs.feedbacks);
+    }, [])
+
+        
+
+    console.log(feedbacks);
+    //console.log(feedbackStore.res);
     
     return (
             <div className="">
                 <h2 className="d-flex m-auto justify-content-center mt-5">Feedback fra Tidligere Praktikanter</h2>
-                {feedbackStore.res.map((el) => 
+                {feedbacks?.map((el) => 
                         <FeedbackCard data={el}/>
                     )}
             </div>
