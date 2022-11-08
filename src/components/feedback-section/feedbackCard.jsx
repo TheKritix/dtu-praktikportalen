133de893bc71ba52647077df25417e38aca2ce18
Kongsbak99 <victoreskongsbak@gmail.com>
2022-11-08 11:04:56 +0100

    post feedback working

--- src/components/feedback-section/feedbackCard.jsx
+++ src/components/feedback-section/feedbackCard.jsx
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import LoginEmployee from "../login/login";
 
 
 const FeedbackCard = ({data}) => {
--- src/components/feedback-section/feedbackSection.jsx
+++ src/components/feedback-section/feedbackSection.jsx
@@ -1,10 +1,13 @@
 import {useState, useEffect} from "react";
-import { observer, inject } from 'mobx-react';
+
+import { observer } from 'mobx-react';
 import "./feedbackStyles.css";
  
 import FeedbackCard from "./feedbackCard";
 //import feedbackData from "./feedbackData";
 import {feedbackStore} from "./feedbackStore";
+import FeedbackInputSection from "./feedbackInputSection";
+
 
 //import FeedbackService from './feedbackService';
 
@@ -25,10 +28,14 @@ const FeedbackSection = () => {
 
     console.log(feedbacks);
     //console.log(feedbackStore.res);
+
+    
     
     return (
             <div className="">
                 <h2 className="d-flex m-auto justify-content-center mt-5">Feedback fra Tidligere Praktikanter</h2>
+                {<FeedbackInputSection/>
+                }
                 {feedbacks?.map((el) => 
                         <FeedbackCard data={el}/>
                     )}
