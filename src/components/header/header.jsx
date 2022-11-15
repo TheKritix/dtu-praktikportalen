4f53fad23adfc564fac58087bdb381bd2c59ca46
oklenum <oliverbrinkklenum@gmail.com>
2022-11-15 08:18:54 +0100

    headerfix

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -30,6 +30,7 @@ const Header = () => {
     }
   }, []);
 
+  /*
   useEffect(() => {
     searchParam.get("ticket");
     if (searchParam.get("ticket") != null && currentUser == null) {
@@ -43,6 +44,7 @@ const Header = () => {
       );
     }
   }, [searchParam, currentUser]);
+  */
 
   return (
     <div>
