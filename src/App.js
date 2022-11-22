d6720ca54abd307832b6405ea9ac5311bdba2dc4
oklenum <oliverbrinkklenum@gmail.com>
2022-11-22 10:35:40 +0100

    navbar and header responsive

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import authService from "./services/auth-service";
 
 // XXXX COMPONENTS XXXX
-import Navbar from "./components/navbar/navbar.jsx";
 import Header from "./components/header/header.jsx";
 import Profile from "./pages/profile";
 import CreatePost from "./pages/createPost";
 import PostPage from "./pages/postPage";
 import InternshipList from "./components/internship-list/internship-list.jsx";
 import EmployerContent from "./components/user-page/usercontent";
+import Navigation from "./components/navbar/navigation.jsx";
 
 function App() {
   document.title = "DTU Praktikportalen";
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Navbar />
+      <Navigation />
       <Routes>
         <Route path="/" element={<Header />} />
         <Route
