2b233b715acdda5ea44a6743c9da68ca441003c1
Oliver Brink Klenum <oliverbrinkklenum@gmail.com>
2022-11-24 23:05:05 +0100

    internshipstore api

--- src/components/internship-list/internship-list-store.jsx
+++ src/components/internship-list/internship-list-store.jsx
@@ -3,6 +3,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 const baseUrl =
   process.env.NODE_ENV === "development" ? "http://localhost:3000/" : ""; //Check if dev environment
 
+const API_URL = process.env.REACT_APP_API;
 class InternshipListStore {
   internships = [];
 
@@ -16,7 +17,7 @@ class InternshipListStore {
   };
 
   fetchInternships() {
-    fetch(baseUrl + "api/post").then((response) =>
+    fetch(API_URL + "post").then((response) =>
       response
         .json()
         .then((json) => runInAction(() => (this.internships = json)))
